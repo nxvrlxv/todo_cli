1f020bc78a3944c39f16a889fe071b4b0ec65902
Eldar <eldar.tishchenko@gmail.com>
2025-08-02 23:21:00 +0300

    add menu

--- todo.py
+++ todo.py
@@ -33,15 +33,70 @@ class TaskManager:
     def add_task(self, task):
         self.tasks.append(task)
 
+    def delete_task(self, task_index):
+        if not self.tasks:
+            return "Нечего удалять, задач нет :3"
+        if task_index < 1 or task_index > len(self.tasks):
+            raise IndexError("Недопустимый индекс! Выберите другой).")
 
+        self.tasks.pop(task_index-1)
+        print(f"Успешно удалена задача {task_index}!")
 
-task = Task("Прогулка на велике", "Прогулка на велике вдоль набережной", "Средний")
-task1 = Task("Занятия спортом", "Поход в тренажерный зал", "Высокий")
-
-
+running = True
 manager = TaskManager()
 
-manager.add_task(task)
-manager.add_task(task1)
+while running:
+    print(f"Добро пожаловать в меню, выберите действие:\n"
+          f"1. Добавить задачу\n"
+          f"2. Показать все задачи\n"
+          f"3. Отметить задачу выполненной\n"
+          f"4. Удалить задачу\n"
+          f"5. Выйти из приложения")
+    number = input()
+    if number == '1':
+        title = input('Введите заголовок задачи')
+        description = input('Введите описание задачи')
+        priority = input('Введите приоритет задачи')
+        task = Task(title, description, priority)
+        manager.add_task(task)
+        input('Нажмите Enter чтобы выйти в меню...')
+        continue
+    if number == '2':
+        print(manager.tasks)
+        input('Нажмите Enter чтобы выйти в меню...')
+        continue
+    if number == '3':
+        if not manager.tasks:
+            print('Нет ни одной добавленной задачи, добавьте, чтобы изменить ее статус.')
+            input('Нажмите Enter чтобы выйти в меню...')
+            continue
+        else:
+            print('Выберите задачу (номер):')
+            print(manager.tasks)
+            number_of_task = int(input())
+            manager.tasks[number_of_task-1].mark_done()
+            input('Нажмите Enter чтобы выйти в меню...')
+            continue
+    if number == '4':
+        print('Выберите задачу (номер), которую хотите удалить:')
+        print(manager.tasks)
+        number_of_task = int(input())
+        manager.delete_task(number_of_task)
+        input('Нажмите Enter чтобы выйти в меню...')
+        continue
+    if number == '5':
+        running = False
 
-print(manager)
+
+#taskk = Task("Прогулка на велике", "Прогулка на велике вдоль набережной", "Средний")
+#task1 = Task("Занятия спортом", "Поход в тренажерный зал", "Высокий")
+
+
+
+
+#manager.add_task(taskk)
+#manager.add_task(task1)
+
+#print(manager)
+#manager.delete_task(100)
+#print(manager.tasks)
